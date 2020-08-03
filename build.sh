ffbf1502621c884900d9e0f4bbe5a6b0937d4a48
Alexis Aguirre <Alexis Aguirre alex08lta@gmail.com>
2020-08-02 21:59:23 -0300

    build

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker build -t alexlndn/raspi-temp .
+docker buildx build --platform linux/arm/v7 -t alexlndn/raspi-temp:armv7 .
+docker buildx build --platform linux/arm64 -t alexlndn/raspi-temp:arm64 .
+
+docker push alexlndn/raspi-temp:armv7
+docker push alexlndn/raspi-temp:arm64
+
+docker manifest create alexlndn/raspi-temp:latest alexlndn/raspi-temp:arm64 alexlndn/raspi-temp:armv7
+docker manifest push alexlndn/raspi-temp:latest
